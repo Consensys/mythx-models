d29533c919ecc64f85d68e812514f1cbb45d813e
Dominik Muhs <dmuhs@protonmail.ch>
2019-11-26 13:54:01 +0100

    Refactor analysis submission request tests

--- tests/test_analysis_submission_request.py
+++ tests/test_analysis_submission_request.py
@@ -5,28 +5,31 @@ import pytest
 from mythx_models.exceptions import ValidationError
 from mythx_models.request import AnalysisSubmissionRequest
 
-from . import common as testdata
+from .common import get_test_case
+
+JSON_DATA, DICT_DATA = get_test_case("testdata/analysis-submission-request.json")
+OBJ_DATA = AnalysisSubmissionRequest.from_json(JSON_DATA)
 
 
 def assert_submission_request(req: AnalysisSubmissionRequest):
-    assert req.contract_name == testdata.CONTRACT_NAME
-    assert req.bytecode == testdata.BYTECODE
-    assert req.main_source == testdata.MAIN_SOURCE
-    assert req.source_map == testdata.SOURCE_MAP
-    assert req.deployed_bytecode == testdata.DEPLOYED_BYTECODE
-    assert req.deployed_source_map == testdata.DEPLOYED_SOURCE_MAP
-    assert req.sources == testdata.SOURCES
-    assert req.solc_version == testdata.SOLC_VERSION
-    assert req.analysis_mode == testdata.ANALYSIS_MODE
+    assert req.contract_name == DICT_DATA["contractName"]
+    assert req.bytecode == DICT_DATA["bytecode"]
+    assert req.main_source == DICT_DATA["mainSource"]
+    assert req.source_map == DICT_DATA["sourceMap"]
+    assert req.deployed_bytecode == DICT_DATA["deployedBytecode"]
+    assert req.deployed_source_map == DICT_DATA["deployedSourceMap"]
+    assert req.sources == DICT_DATA["sources"]
+    assert req.solc_version == DICT_DATA["version"]
+    assert req.analysis_mode == DICT_DATA["analysisMode"]
     assert req.method == "POST"
     assert req.headers == {}
     assert req.parameters == {}
-    assert req.payload == {"data": testdata.ANALYSIS_SUBMISSION_REQUEST_DICT}
+    assert req.payload == {"data": DICT_DATA}
 
 
 def test_analysis_submission_request_from_valid_json():
     req = AnalysisSubmissionRequest.from_json(
-        json.dumps(testdata.ANALYSIS_SUBMISSION_REQUEST_DICT)
+        JSON_DATA
     )
     assert_submission_request(req)
 
@@ -37,7 +40,7 @@ def test_analysis_submission_request_from_invalid_json():
 
 
 def test_analysis_submission_request_from_valid_dict():
-    req = AnalysisSubmissionRequest.from_dict(testdata.ANALYSIS_SUBMISSION_REQUEST_DICT)
+    req = AnalysisSubmissionRequest.from_dict(DICT_DATA)
     assert_submission_request(req)
 
 
@@ -48,42 +51,42 @@ def test_analysis_submission_request_from_invalid_dict():
 
 def test_analysis_submission_request_to_json():
     assert (
-        json.loads(testdata.ANALYSIS_SUBMISSION_REQUEST_OBJECT.to_json())
-        == testdata.ANALYSIS_SUBMISSION_REQUEST_DICT
+        json.loads(OBJ_DATA.to_json())
+        == DICT_DATA
     )
 
 
 def test_analysis_submission_request_to_dict():
     assert (
-        testdata.ANALYSIS_SUBMISSION_REQUEST_OBJECT.to_dict()
-        == testdata.ANALYSIS_SUBMISSION_REQUEST_DICT
+        OBJ_DATA.to_dict()
+        == DICT_DATA
     )
 
 
 def test_analysis_submission_request_bytecode_only():
-    req = AnalysisSubmissionRequest(bytecode=testdata.BYTECODE)
-    assert req.bytecode == testdata.BYTECODE
+    req = AnalysisSubmissionRequest(bytecode=DICT_DATA["bytecode"])
+    assert req.bytecode == DICT_DATA["bytecode"]
     assert req.analysis_mode == "quick"  # default value
-    assert req.to_dict() == {"bytecode": testdata.BYTECODE, "analysisMode": "quick"}
+    assert req.to_dict() == {"bytecode": DICT_DATA["bytecode"], "analysisMode": "quick"}
     assert json.loads(req.to_json()) == {
-        "bytecode": testdata.BYTECODE,
+        "bytecode": DICT_DATA["bytecode"],
         "analysisMode": "quick",
     }
 
 
 def test_analysis_submission_request_source_only():
-    req = AnalysisSubmissionRequest(sources=testdata.SOURCES)
-    assert req.sources == testdata.SOURCES
+    req = AnalysisSubmissionRequest(sources=DICT_DATA["sources"])
+    assert req.sources == DICT_DATA["sources"]
     assert req.analysis_mode == "quick"  # default value
-    assert req.to_dict() == {"sources": testdata.SOURCES, "analysisMode": "quick"}
+    assert req.to_dict() == {"sources": DICT_DATA["sources"], "analysisMode": "quick"}
     assert json.loads(req.to_json()) == {
-        "sources": testdata.SOURCES,
+        "sources": DICT_DATA["sources"],
         "analysisMode": "quick",
     }
 
 
 def test_analysis_submission_request_invalid_mode():
-    req = AnalysisSubmissionRequest(bytecode=testdata.BYTECODE, analysis_mode="invalid")
+    req = AnalysisSubmissionRequest(bytecode=DICT_DATA["bytecode"], analysis_mode="invalid")
     with pytest.raises(ValidationError):
         req.to_dict()
 
