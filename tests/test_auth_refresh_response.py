170d0867f3d9cd29b825e76a725feb995358cbcf
Dominik Muhs <dmuhs@protonmail.ch>
2019-11-26 15:08:14 +0100

    Refactor auth refresh response tests

--- tests/test_auth_refresh_response.py
+++ tests/test_auth_refresh_response.py
@@ -5,23 +5,19 @@ import pytest
 from mythx_models.exceptions import ValidationError
 from mythx_models.response import AuthRefreshResponse
 
-from . import common as testdata
+from .common import get_test_case
 
-ACCESS_TOKEN = "my_fancy_access_token"
-REFRESH_TOKEN = "my_fancy_refresh_token"
-AUTH_REFRESH = {"access": ACCESS_TOKEN, "refresh": REFRESH_TOKEN}
-AUTH_REFRESH_RESPONSE = AuthRefreshResponse(
-    access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN
-)
+JSON_DATA, DICT_DATA = get_test_case("testdata/auth-refresh-response.json")
+OBJ_DATA = AuthRefreshResponse.from_json(JSON_DATA)
 
 
 def assert_auth_refresh_response(resp: AuthRefreshResponse):
-    assert resp.access_token == testdata.ACCESS_TOKEN_1
-    assert resp.refresh_token == testdata.REFRESH_TOKEN_1
+    assert resp.access_token == DICT_DATA["jwtTokens"]["access"]
+    assert resp.refresh_token == DICT_DATA["jwtTokens"]["refresh"]
 
 
 def test_auth_refresh_response_from_valid_json():
-    resp = AuthRefreshResponse.from_json(json.dumps(testdata.REFRESH_RESPONSE_DICT))
+    resp = AuthRefreshResponse.from_json(JSON_DATA)
     assert_auth_refresh_response(resp)
 
 
@@ -31,7 +27,7 @@ def test_auth_refresh_response_from_invalid_json():
 
 
 def test_auth_refresh_response_from_valid_dict():
-    resp = AuthRefreshResponse.from_dict(testdata.REFRESH_RESPONSE_DICT)
+    resp = AuthRefreshResponse.from_dict(DICT_DATA)
     assert_auth_refresh_response(resp)
 
 
@@ -41,11 +37,8 @@ def test_auth_refresh_response_from_invalid_dict():
 
 
 def test_auth_refresh_response_to_json():
-    assert (
-        json.loads(testdata.REFRESH_RESPONSE_OBJECT.to_json())
-        == testdata.REFRESH_RESPONSE_DICT
-    )
+    assert json.loads(OBJ_DATA.to_json()) == DICT_DATA
 
 
 def test_auth_refresh_response_to_dict():
-    assert testdata.REFRESH_RESPONSE_OBJECT.to_dict() == testdata.REFRESH_RESPONSE_DICT
+    assert OBJ_DATA.to_dict() == DICT_DATA
