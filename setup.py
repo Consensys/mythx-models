b7882a8dcef64750625cee9bae4ac0cb1e867a26
Dominik Muhs <dmuhs@protonmail.ch>
2020-02-28 17:39:32 +0100

    Bump version: 1.7.3 → 1.7.4

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dmuhs/mythx-models",
-    version="1.7.3",
+    version="1.7.4",
     zip_safe=False,
 )
