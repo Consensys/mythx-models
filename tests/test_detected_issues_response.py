f4161c050492b001750e1038d854254df8676fb0
Dominik Muhs <dmuhs@protonmail.ch>
2020-12-14 21:16:07 +0100

    Don't fail on slow data generation

--- tests/test_detected_issues_response.py
+++ tests/test_detected_issues_response.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from hypothesis import given
+from hypothesis import given, settings, HealthCheck
 from pydantic import parse_obj_as
 
 from mythx_models.response import IssueReport
@@ -9,6 +9,7 @@ from .strategies.report import detected_issues_response
 
 
 @given(detected_issues_response())
+@settings(suppress_health_check=[HealthCheck.too_slow])
 def test_serde(response):
     objs = parse_obj_as(List[IssueReport], response)
     assert [obj.dict(by_alias=True) for obj in objs] == response
