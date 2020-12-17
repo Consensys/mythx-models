dfc119c25b45ea3b8da47b7d3c0506b0752c8b7b
Dominik Muhs <dmuhs@protonmail.ch>
2020-12-17 11:05:57 +0100

    Bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dmuhs/mythx-models",
-    version="1.9.3",
+    version="2.0.0",
     zip_safe=False,
 )
