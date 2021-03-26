391f94fb934c600cc39f32b4d9933747661ecd86
Dominik Muhs <dmuhs@protonmail.ch>
2021-03-26 17:33:02 +0100

    Bump version: 2.1.0 → 2.1.1

--- mythx_models/__init__.py
+++ mythx_models/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Dominik Muhs"""
 __email__ = "dominik.muhs@consensys.net"
-__version__ = "2.1.0"
+__version__ = "2.1.1"
--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dmuhs/mythx-models",
-    version="2.1.0",
+    version="2.1.1",
     zip_safe=False,
 )
